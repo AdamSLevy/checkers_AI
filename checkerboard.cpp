70c86c74209b91debb1f9ea7bf341eb06222b41e
Adam S Levy <theadamlevy@gmail.com>
2016-03-31 19:02:33 -0800

    Added bit_count

--- checkerboard.cpp
+++ checkerboard.cpp
@@ -3,6 +3,13 @@
 
 #include "checkerboard.hpp"
 
+size_t bit_count(uint32_t i)
+{
+     i = i - ((i >> 1) & 0x55555555);
+     i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
+     return (((i + (i >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24;
+}
+
 uint32_t get_movers(BitBoard const & bb, bool kings)/*{{{*/
 {
     uint32_t play_pos;
